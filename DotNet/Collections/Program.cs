ce8a153d6954ef4c4df954947a42d630cc64688d
Oleg Novoselskyy <oleg.novoselskyy@gmail.com>
2019-07-14 17:10:19 -0400

    RD-6

--- DotNet/Collections/Program.cs
+++ DotNet/Collections/Program.cs
@@ -18,10 +18,19 @@ namespace Collections
                 "Sunday"
             };
 
-            foreach (string day in daysOfWeek)
-            {
-                Console.WriteLine(day);
-            }
+            //foreach (string day in daysOfWeek)
+            //{
+            //    Console.WriteLine(day);
+            //}
+
+            //Console.WriteLine(daysOfWeek[1]);
+
+
+            Console.WriteLine("What day of the week would you like to display? (1-7)");
+            var input = Console.ReadLine();
+            var day = Convert.ToInt32(input) - 1;
+
+            Console.WriteLine($"That day is {daysOfWeek[day]}");
         }
     }
 }
