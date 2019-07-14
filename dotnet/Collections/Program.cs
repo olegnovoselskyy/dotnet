89ec04310da2f81c4393783067d78ce220d373c8
Dmytro Lytvyn <dlytpi@gmail.com>
2019-07-14 17:10:29 -0400

    RD-8

--- dotnet/Collections/Program.cs
+++ dotnet/Collections/Program.cs
@@ -5,8 +5,23 @@ namespace Collections
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        {       
+            string[] daysOfWeek =
+            {
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday"
+            };
+
+            Console.WriteLine("What day is it?");
+            int inputtedDay = int.Parse(Console.ReadLine()) - 1;
+
+            string chosenDay = daysOfWeek[inputtedDay];
+            Console.WriteLine($"Today is {chosenDay}");
         }
     }
 }
