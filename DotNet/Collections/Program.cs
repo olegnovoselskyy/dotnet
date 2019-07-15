02e03633ed723d10978b75dcb60ca7380c45db20
Oleg Novoselskyy <oleg.novoselskyy@gmail.com>
2019-07-14 20:19:05 -0400

    RD-6

--- DotNet/Collections/Program.cs
+++ DotNet/Collections/Program.cs
@@ -7,31 +7,35 @@ namespace Collections
         static void Main(string[] args)
         {
 
-            string[] daysOfWeek =
-            {
-                "Monday",
-                "Tuesday",
-                "Wednesday",
-                "Thursday",
-                "Friday",
-                "Saturday",
-                "Sunday"
-            };
 
-            //         daysOfWeek[6] = "Day of the LORD!"; 
-            int count = 0;
-            foreach (string day in daysOfWeek)
-            {
-                
-                if (count != 2)
-                {
-                    daysOfWeek[count] = "LAST DAY OF DIRT!";
-                }
-                
-                Console.WriteLine(daysOfWeek[count]);
-                count++;
-            }
-         
+
+            #region Old Code
+
+            //string[] daysOfWeek =
+            //{
+            //    "Monday",
+            //    "Tuesday",
+            //    "Wednesday",
+            //    "Thursday",
+            //    "Friday",
+            //    "Saturday",
+            //    "Sunday"
+            //};
+
+            //// daysOfWeek[6] = "Day of the LORD!"; 
+            //int count = 0;
+            //foreach (string day in daysOfWeek)
+            //{
+
+            //    if (count != 2)
+            //    {
+            //        daysOfWeek[count] = "LAST DAY OF DIRT!";
+            //    }
+
+            //    Console.WriteLine(daysOfWeek[count]);
+            //    count++;
+            //}
+
 
             //Console.WriteLine(daysOfWeek[1]);
 
@@ -39,6 +43,9 @@ namespace Collections
             //Console.WriteLine("What day of the week would you like to display? (1-7)");
             //var input = Console.ReadLine();         
             //Console.WriteLine($"That day is {daysOfWeek[Convert.ToInt32(input)-1]}");
+
+            #endregion
+
         }
     }
 }
