bb0b8f51db20b714fd3a22b9ec85d4ea4fb06af4
Dmytro Lytvyn <dlytpi@gmail.com>
2019-07-15 21:17:11 -0400

    RD-8 Insert into list by index

--- dotnet/Collections/Program.cs
+++ dotnet/Collections/Program.cs
@@ -43,11 +43,15 @@ namespace Collections
             CsvReader reader = new CsvReader(filePath);
 
             List<Country> countries = reader.ReadAllCountries();
+            Country lilliput = new Country("Lilliput", "LIL", "Somewhere", 2_000_000);
+            int lilliputIndex = countries.FindIndex(x=>x.Population < 2_000_000);
+            countries.Insert(lilliputIndex, lilliput);
 
             foreach (Country country in countries)
             {
                 Console.WriteLine($"{PopulationFormatter.FormatPopulation(country.Population).PadLeft(15)}: {country.Name}");
             }
+            Console.WriteLine($"{countries.Count} countries");
         }
     }
 }
