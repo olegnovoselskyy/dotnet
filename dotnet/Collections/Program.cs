dde108fcedaa1f41980d7dd364990e8c32859bc8
Dmytro Lytvyn <dlytpi@gmail.com>
2019-07-15 21:50:06 -0400

    RD-8 simplified initialization

--- dotnet/Collections/Program.cs
+++ dotnet/Collections/Program.cs
@@ -57,9 +57,11 @@ namespace Collections
             Country norway = new Country("Norway", "NOR", "Europe", 5_282_223);
             Country finland = new Country("Finland", "FIN", "Europe", 5_282_223);
 
-            var countries = new Dictionary<string, Country>();
-            countries.Add(norway.Code, norway);
-            countries.Add(finland.Code, finland);
+            var countries = new Dictionary<string, Country>
+            {
+                { norway.Code, norway },
+                { finland.Code, finland }
+            };
 
             Country selectedCountry = countries.ContainsKey(norway.Code) ? countries[norway.Code] : null;
 
