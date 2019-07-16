bea39e78da8c840355f5226367921951da2767ce
Dmytro Lytvyn <dlytpi@gmail.com>
2019-07-15 21:45:08 -0400

    RD-8 dictionary enumerating and finding by key

--- dotnet/Collections/Program.cs
+++ dotnet/Collections/Program.cs
@@ -39,20 +39,41 @@ namespace Collections
             //int indexDay = int.Parse(Console.ReadLine()) - 1;
             //Console.WriteLine($"Today is {daysOfWeek[indexDay]}");
 
-            string filePath = @"C:\Users\amid2\source\repos\dotnet\dotnet\Collections\Pop by Largest Final.csv";
-            CsvReader reader = new CsvReader(filePath);
+            //string filePath = @"C:\Users\amid2\source\repos\dotnet\dotnet\Collections\Pop by Largest Final.csv";
+            //CsvReader reader = new CsvReader(filePath);
 
-            List<Country> countries = reader.ReadAllCountries();
-            Country lilliput = new Country("Lilliput", "LIL", "Somewhere", 2_000_000);
-            int lilliputIndex = countries.FindIndex(x=>x.Population < 2_000_000);
-            countries.Insert(lilliputIndex, lilliput);
-            countries.RemoveAt(lilliputIndex);
+            //List<Country> countries = reader.ReadAllCountries();
+            //Country lilliput = new Country("Lilliput", "LIL", "Somewhere", 2_000_000);
+            //int lilliputIndex = countries.FindIndex(x=>x.Population < 2_000_000);
+            //countries.Insert(lilliputIndex, lilliput);
+            //countries.RemoveAt(lilliputIndex);
 
-            foreach (Country country in countries)
-            {
-                Console.WriteLine($"{PopulationFormatter.FormatPopulation(country.Population).PadLeft(15)}: {country.Name}");
-            }
-            Console.WriteLine($"{countries.Count} countries");
+            //foreach (Country country in countries)
+            //{
+            //    Console.WriteLine($"{PopulationFormatter.FormatPopulation(country.Population).PadLeft(15)}: {country.Name}");
+            //}
+            //Console.WriteLine($"{countries.Count} countries");
+
+            Country norway = new Country("Norway", "NOR", "Europe", 5_282_223);
+            Country finland = new Country("Finland", "FIN", "Europe", 5_282_223);
+
+            var countries = new Dictionary<string, Country>();
+            countries.Add(norway.Code, norway);
+            countries.Add(finland.Code, finland);
+
+            Country selectedCountry = countries.ContainsKey(norway.Code) ? countries[norway.Code] : null;
+
+            if (selectedCountry != null)
+                Console.WriteLine($"Selected Country: {selectedCountry.Name}");
+
+            Console.WriteLine();
+            Console.WriteLine("All Countries:");
+            foreach (var country in countries.Values) 
+                Console.WriteLine(country.Name);
+            
+
+
+
         }
     }
 }
