f8f49b28b40b406c77c96065b2f8b4817769b539
Oleg Novoselskyy <oleg.novoselskyy@gmail.com>
2019-07-16 20:04:34 -0400

    RD-6

---
+++ DotNet/Collections/CsvReader.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Collections
+{
+    class CsvReader
+    {
+        private string _csvFilePath;
+
+        public CsvReader(string csvFilePath)
+        {
+            _csvFilePath = csvFilePath;
+        }
+
+        public Country[] ReadFirstNCountries(int nCountries)
+        {
+            return null;
+        }
+    }
+}
