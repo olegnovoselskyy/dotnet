6bd29f336753df2675a119dfbec8cd553c20da30
Dmytro Lytvyn <dlytpi@gmail.com>
2019-07-25 22:30:58 -0400

    RD-9 Destructor

--- dotnet/WorkingWithNullsCSharp/Program.cs
+++ dotnet/WorkingWithNullsCSharp/Program.cs
@@ -8,6 +8,8 @@ namespace WorkingWithNullsCSharp
         {
             var player = new PlayerCharacter();
             player.Name = "Destructor";
+            player.DaysSinceLastLogin = 1;
+            player.DateOfBirth = Convert.ToDateTime("10/31/1913 3:00 AM");
 
             PlayerDisplay.Write(player);
             Console.ReadLine();
