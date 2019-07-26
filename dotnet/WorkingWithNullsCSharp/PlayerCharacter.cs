f145aea44fe700d9ff4a53486104cfc007640e96
Dmytro Lytvyn <dlytpi@gmail.com>
2019-07-25 22:41:13 -0400

    RD-9 default value Nullable<T> is NULL

--- dotnet/WorkingWithNullsCSharp/PlayerCharacter.cs
+++ dotnet/WorkingWithNullsCSharp/PlayerCharacter.cs
@@ -10,11 +10,5 @@ namespace WorkingWithNullsCSharp
         public int? DaysSinceLastLogin { get; set; }
         public DateTime? DateOfBirth { get; set; }
         public bool? IsNoob { get; set; }
-
-        public PlayerCharacter()
-        {
-            DateOfBirth = null;
-            DaysSinceLastLogin = null;
-        }
     }
 }
