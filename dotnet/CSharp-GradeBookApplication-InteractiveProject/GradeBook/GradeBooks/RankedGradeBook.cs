bd5a63f523ee680638392dc580b799538443c791
Dmytro Lytvyn <dlytpi@gmail.com>
2019-07-25 22:06:01 -0400

    RD-10

---
+++ dotnet/CSharp-GradeBookApplication-InteractiveProject/GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    public class RankedGradeBook : BaseGradeBook
+    {
+        public RankedGradeBook(string name, bool weighted) : base(name, weighted)
+        {
+            Type = Enums.GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if(Students.Count < 5)
+                throw new InvalidOperationException();
+
+            var threshold = (int)Math.Ceiling(Students.Count * 0.2);
+            var grades = Students.OrderByDescending(x => x.AverageGrade).Select(x => x.AverageGrade).ToList();
+
+            if (grades[threshold - 1] <= averageGrade)
+                return 'A';
+            else if (grades[(threshold * 2) - 1] <= averageGrade)
+                return 'B';
+            else if (grades[(threshold * 3) - 1] <= averageGrade)
+                return 'C';
+            else if (grades[(threshold * 4) - 1] <= averageGrade)
+                return 'D';
+            else
+                return 'F';
+        }
+
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+            else
+                base.CalculateStatistics();
+
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+            else
+                base.CalculateStudentStatistics(name);
+        }
+    }
+}
