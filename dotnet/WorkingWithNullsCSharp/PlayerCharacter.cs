ad342c343f94490355381eab593d04ce4a81b9e0
Dmytro Lytvyn <dlytpi@gmail.com>
2019-07-25 22:35:19 -0400

    RD-9 Nullable<T> Shorthand - ?

--- dotnet/WorkingWithNullsCSharp/PlayerCharacter.cs
+++ dotnet/WorkingWithNullsCSharp/PlayerCharacter.cs
@@ -7,8 +7,8 @@ namespace WorkingWithNullsCSharp
     class PlayerCharacter
     {
         public string Name { get; set; }
-        public Nullable<int> DaysSinceLastLogin { get; set; }
-        public Nullable<DateTime> DateOfBirth { get; set; }
+        public int? DaysSinceLastLogin { get; set; }
+        public DateTime? DateOfBirth { get; set; }
 
         public PlayerCharacter()
         {
