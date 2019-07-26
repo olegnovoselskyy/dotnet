b1abf01f1ad21b4738582ca3ec135a8561a2a881
Dmytro Lytvyn <dlytpi@gmail.com>
2019-07-25 23:49:02 -0400

    RD-9 null-conditional operator

--- dotnet/WorkingWithNullsCSharp/Program.cs
+++ dotnet/WorkingWithNullsCSharp/Program.cs
@@ -6,12 +6,25 @@ namespace WorkingWithNullsCSharp
     {
         static void Main(string[] args)
         {
-            var player = new PlayerCharacter();
-            player.Name = "Young Dim Flexmaster Floss";
+            PlayerCharacter player = null;
+            //player.Name = "Young Dim Flexmaster Floss";
             //player.DaysSinceLastLogin = 1;
-            player.DateOfBirth = Convert.ToDateTime("10/31/1913 3:00 AM");
+            //player.DateOfBirth = Convert.ToDateTime("10/31/1913 3:00 AM");
 
-            PlayerDisplay.Write(player);
+            int days;
+
+            if(player != null)
+            {
+                days = player.DaysSinceLastLogin ?? -1;
+            }
+            else
+            {
+                days = -1;
+            }
+
+            Console.WriteLine(days);
+
+            //PlayerDisplay.Write(player);
             Console.ReadLine();
         }
     }
