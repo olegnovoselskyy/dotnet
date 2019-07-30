62bf98d7e63bf6525b42bca6f28c7a7bd7454ebc
Oleg Novoselskyy <oleg.novoselskyy@gmail.com>
2019-07-30 19:57:17 -0400

    RD-14

--- DotNet/ACM.BL/Customer.cs
+++ DotNet/ACM.BL/Customer.cs
@@ -4,5 +4,53 @@ namespace ACM.BL
 {
     public class Customer
     {
+
+        #region Fields
+
+        private string _firstName;
+        private string _lastName;
+
+        #endregion
+
+        #region Properties
+
+        public int CustomerID { get; private set; }
+
+        public string FirstName
+        {
+            get
+            {
+                return _firstName;
+            }
+            set
+            {
+                _firstName = value;
+            }
+        }
+        public string LastName
+        {
+            get
+            {
+                return _lastName;
+            }
+            set
+            {
+                _lastName = value;
+            }
+        }
+
+        public string EmailAddress { get; set; }
+
+        public string FullName
+        {
+            get
+            {
+                return FirstName + " " + LastName;
+            }
+        }
+
+
+        #endregion
+
     }
 }
