188748b717fe0be64e25fcd70babc49ac4852263
Dmytro Lytvyn <dlytpi@gmail.com>
2019-07-31 22:41:08 -0400

    RD-12 Order Test Method taken out of CustometTest.cs and put into new file - OrderTest.cs

--- dotnet/OOPFundamentalsTest/OOPFundamentals.BLTest/CustomerTest.cs
+++ dotnet/OOPFundamentalsTest/OOPFundamentals.BLTest/CustomerTest.cs
@@ -103,62 +103,6 @@ namespace OOPFundamentals.BLTest
 
             //--Assert
             Assert.AreEqual(expected, actual);
-        }
-
-        [TestMethod]
-        public void ValidateOrder()
-        {
-            bool OrderValid = true;
-            int orderItemCount = 0;
-            Customer luckyCustomer1 = new Customer
-            {
-                FirstName = "Ya",
-                LastName = "Boi",
-                EmailAddress = "yaboi@skyylyt.com"
-
-            };
-            Customer.InstanceCount++;
-
-            Product SkyyLytGame = new Product
-            {
-                ProductID = 1,
-                ProductName = "Pocket Pals: Adventure",
-                Description = "A game for friends, with friends.",
-                CurrentPrice = 20
-            };
-
-            OrderItem oi1 = new OrderItem
-            {
-                OrderItemID = 1,
-                ProductID = SkyyLytGame.ProductID,
-                Quantity = 1,
-                PurchasePrice = 20
-            };
-            orderItemCount++;
-
-            Order o1 = new Order
-            {
-                OrderID = 1,
-                OrderDate = DateTime.Now,
-                CustomerID = luckyCustomer1.CustomerID,
-                OrderItemCount = orderItemCount
-            };
-
-            if(luckyCustomer1.Validate() && SkyyLytGame.Validate() && oi1.Validate() && o1.Validate())
-            {
-                OrderValid = true;
-            }
-            else
-            {
-                OrderValid = false;
-            }
-
-            var expected = true;
-
-            var actual = OrderValid;
-
-            Assert.AreEqual(expected, actual);
-        }
-
+        } 
     }
 }
