158dd26314d10c04bcfa653f486dbd47ef923572
Dmytro Lytvyn <dlytpi@gmail.com>
2019-08-03 15:38:37 -0400

    RD-12 using the Loggable Service, test - Interface-based Polymorphism

---
+++ dotnet/OOPFundamentals.CommonTest/LoggingServiceTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OOPFundamentals.BL;
+using OOPFundamentals.Common;
+
+namespace OOPFundamentals.CommonTest
+{
+    [TestClass]
+    public class LoggingServiceTest
+    {
+        [TestMethod]
+        public void WriteToFileTest()
+        {
+            // Arrange
+            var changedItems = new List<ILoggable>();
+
+            var customer = new Customer(1)
+            {
+                FirstName = "D",
+                LastName = "L",
+                EmailAddress = "d@l.com",
+                AddressList = null
+            };
+            changedItems.Add(customer);
+
+            var product = new Product(2)
+            {
+                ProductName = "Keyboard",
+                Description = "Type with this.",
+                CurrentPrice = 80M
+            };
+            changedItems.Add(product);
+
+            // Act
+            LoggingService.WriteToFile(changedItems);
+
+            // Assert
+            // Nothing to assert
+        }
+    }
+}
