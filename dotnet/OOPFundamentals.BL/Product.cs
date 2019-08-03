692b3b23a878c200229734d1db212402169518a7
Dmytro Lytvyn <dlytpi@gmail.com>
2019-08-03 14:46:56 -0400

    RD-12 reusable string handler component

--- dotnet/OOPFundamentals.BL/Product.cs
+++ dotnet/OOPFundamentals.BL/Product.cs
@@ -19,10 +19,20 @@ namespace OOPFundamentals.BL
         }
 
         public int ProductID { get; set; }
-        public string ProductName { get; set; }
+        private string _productName;
+        public string ProductName
+        {
+            get
+            {
+                return _productName;
+            }
+            set
+            {
+                _productName = value;
+            }
+        }
         public string Description { get; set; }
         public decimal? CurrentPrice { get; set; }
-
         public override string ToString() => ProductName;
 
         /// <summary>
