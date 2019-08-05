1c344a0dea17714cd503f1a50543f6d69d4cf74d
Dmytro Lytvyn <dlytpi@gmail.com>
2019-08-04 22:41:55 -0400

    RD-13 including time zone

--- dotnet/DatesAndTimes/Program.cs
+++ dotnet/DatesAndTimes/Program.cs
@@ -9,12 +9,12 @@ namespace DatesAndTimes
     {
         static void Main(string[] args)
         {
+            // Parsing Date Time from file
             var lines = File.ReadAllLines(@"C:\Users\amid2\source\repos\dotnet\dotnet\DatesAndTimes\StockData.csv");
 
             Console.WriteLine("Dates/Times from file:");
             foreach (var line in lines.Skip(1))
             {
-                // Parsing Date Time from file
                 var segments = line.Split(',');
                 var tradeDate = DateTime.Parse(segments[1], CultureInfo.GetCultureInfo("en-GB"));
 
@@ -25,11 +25,16 @@ namespace DatesAndTimes
             // Converting Between Time Zones
             var now = DateTime.Now;
             TimeZoneInfo sydneyTimeZone = TimeZoneInfo.FindSystemTimeZoneById("E. Australia Standard Time");
-
             var sydneyTime = TimeZoneInfo.ConvertTime(now, sydneyTimeZone);
+
             Console.WriteLine("Converting Between Time Zones:");
             Console.WriteLine($"Local: {now}");
             Console.WriteLine($"Sydney: {sydneyTime}");
+
+            // DateTimeOffset includes TimeZone
+            Console.WriteLine();
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine(DateTimeOffset.Now);
 
         }
     }
