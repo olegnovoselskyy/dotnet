50a4b829fda84dd5d20656325e85926f8fccdad3
Oleg Novoselskyy <oleg.novoselskyy@gmail.com>
2019-08-05 18:56:03 -0400

    RD-15

---
+++ DotNet/DatesAndTimes/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace DatesAndTimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines(@"\Repos\dotnet\DotNet\Data\StockData.csv");
+
+            foreach (var line in lines.Skip(1))
+            {
+                var segments = line.Split(',');
+
+                var tradeDate = DateTime.Parse(segments[1]);
+
+                Console.WriteLine(tradeDate.ToLongDateString());
+            }
+        }
+    }
+}
