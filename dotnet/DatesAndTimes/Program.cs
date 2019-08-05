1fb67f58512487c888de72becae0408b3bb9c044
Dmytro Lytvyn <dlytpi@gmail.com>
2019-08-04 22:23:47 -0400

    RD-13 Read from file and Parse Date Time

---
+++ dotnet/DatesAndTimes/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace DatesAndTimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines(@"C:\Users\amid2\source\repos\dotnet\dotnet\DatesAndTimes\StockData.csv");
+
+            foreach (var line in lines.Skip(1))
+            {
+                var segments = line.Split(',');
+                var tradeDate = DateTime.Parse(segments[1]);
+
+                Console.WriteLine(tradeDate.ToLongDateString());
+
+            }
+        }
+    }
+}
