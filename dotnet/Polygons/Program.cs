2114ada90f150c246f5efa28a81ac69880d70b78
Dmytro Lytvyn <dlytpi@gmail.com>
2019-08-06 12:59:33 -0400

    RD-19 Setup

---
+++ dotnet/Polygons/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Polygons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
