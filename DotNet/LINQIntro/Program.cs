c78b9d7af2b90f05b2a16ab127667fd162dcb3b6
Oleg Novoselskyy <oleg.novoselskyy@gmail.com>
2019-08-07 19:40:56 -0400

    RD-17

---
+++ DotNet/LINQIntro/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace LINQIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\windows";
+            ShowLargeFilesWithoutLinq(path);
+        }
+
+        private static void ShowLargeFilesWithoutLinq(string path)
+        {
+            DirectoryInfo dir = new DirectoryInfo(path);
+            var files = dir.GetFiles();
+
+            foreach (var file in files)
+            {
+                Console.WriteLine($"{file.Length}");
+            }
+        }
+    }
+}
