458c346429a23e2f5e5a4afffa301bd4026d9de7
Dmytro Lytvyn <dlytpi@gmail.com>
2019-08-08 12:54:19 -0400

    RD-20 Writing largest files to Console without Linq

---
+++ dotnet/Introduction/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introduction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Program Files (x86)\Image-Line\FL Studio 20\Data\Patches\Packs\PACKS\various samples\random stuff and loops";
+            ShowLargeFilesWithoutLinq(path);
+
+            Console.Read();
+
+        }
+
+        private static void ShowLargeFilesWithoutLinq(string path)
+        {
+            DirectoryInfo directory = new DirectoryInfo(path);
+            FileInfo[] files = directory.GetFiles();
+            Array.Sort(files, new FileInfoComparer());
+
+            for (int i = 0; i < 5; i++)
+            {
+                FileInfo file = files[i];
+                Console.WriteLine($"{file.Name, -20} - {file.Length, 10:N0}");
+            }
+        }
+
+        public class FileInfoComparer : Comparer<FileInfo>
+        {
+            public override int Compare(FileInfo x, FileInfo y)
+            {
+                return y.Length.CompareTo(x.Length);
+            }
+        }
+    }
+}
