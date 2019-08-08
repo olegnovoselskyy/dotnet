0af6743bddba764a2ac00fe3e5143c53c2651256
Dmytro Lytvyn <dlytpi@gmail.com>
2019-08-08 19:46:10 -0400

    RD-20 Extension Method on IEnumerable<T> Count

---
+++ dotnet/Features/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Features
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee[] developers = new Employee[]
+            {
+                new Employee {Id = 1, Name="Dima"},
+                new Employee {Id = 1, Name="Dima"},
+            };
+
+            List<Employee> sales = new List<Employee>()
+            {
+                new Employee {Id = 3, Name="Alex"}
+            };
+
+            Console.WriteLine(developers.Count());
+        }
+    }
+}
