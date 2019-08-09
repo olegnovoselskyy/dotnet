753450622606dac57f33b5b776655b9d31b42f0d
Dmytro Lytvyn <dlytpi@gmail.com>
2019-08-08 20:58:50 -0400

    RD-20 Count does not use deferred execution

--- dotnet/Queries/Program.cs
+++ dotnet/Queries/Program.cs
@@ -16,8 +16,9 @@ namespace Queries
                 new Movie {Title = "Star Wars V", Rating = 8.7f, Year= 1980}
             };
 
-            var query = movies.Where(m => m.Year > 2000).Take(1); 
+            var query = movies.Where(m => m.Year > 2000).Take(1);
 
+            Console.WriteLine(query.Count());
             var enumerator = query.GetEnumerator();
             while (enumerator.MoveNext())
             {
