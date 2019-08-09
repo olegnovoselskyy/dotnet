3fb3a8da0e6b755111b525a61ba77da0b4081111
Dmytro Lytvyn <dlytpi@gmail.com>
2019-08-08 20:39:46 -0400

    RD-20 custom filter operator

---
+++ dotnet/Queries/Movie.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Queries
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public double Rating { get; set; }
+        public int Year { get; set; }
+    }
+}
