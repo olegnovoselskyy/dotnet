fb1f45e083abd3177a70ed1af9a232eb1c2581ce
Dmytro Lytvyn <dlytpi@gmail.com>
2019-08-08 20:44:53 -0400

    RD-20 Linq behaves differently, does not iterate when it does not need to

--- dotnet/Queries/Program.cs
+++ dotnet/Queries/Program.cs
@@ -16,7 +16,7 @@ namespace Queries
                 new Movie {Title = "Star Wars V", Rating = 8.7f, Year= 1980}
             };
 
-            var query = movies.Filter(m => m.Year > 2000);
+            var query = movies.Where(m => m.Year > 2000);
 
             foreach (var movie in query)
             {
