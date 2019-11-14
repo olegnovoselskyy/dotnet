86cb5b53296f342216924fc673f675efb1c56468
Oleg Novoselskyy <oleg.novoselskyy@gmail.com>
2019-11-13 19:34:38 -0500

    RD-18 - Finish Defensive Coding in C#

--- DotNet/Queries/Program.cs
+++ DotNet/Queries/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Queries
 {
@@ -6,7 +8,18 @@ namespace Queries
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var movies = new List<Movie>()
+            {
+                new Movie { Title = "The Dark Knight", Rating = 9.0f, Year = 2008},
+                new Movie { Title = "Fight Club", Rating = 8.8f, Year = 1999},
+                new Movie { Title = "Star Wars V", Rating = 8.7f, Year = 1980},
+                new Movie { Title = "Avengers: Endgame", Rating = 8.5f, Year = 2019}
+            };
+
+            foreach (var movie in movies.Filter(m => m.Year > 2000))
+            {
+                Console.WriteLine(movie.Title);
+            }
         }
     }
 }
