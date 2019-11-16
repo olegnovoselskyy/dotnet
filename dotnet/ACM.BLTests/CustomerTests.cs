0e01caf51d5c4d992e99872d3963be183381b348
Dmytro Lytvyn <dlytpi@gmail.com>
2019-11-16 13:39:09 -0500

    RD-21 Unit Testing

---
+++ dotnet/ACM.BLTests/CustomerTests.cs
@@ -0,0 +1,95 @@
+using System;
+using ACM.BL;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace ACM.BLTests
+{
+    [TestClass()]
+    public class CustomerTests
+    {
+        [TestMethod]
+        public void CalculatePercentOfGoalStepsTestValid()
+        {
+            //-- Arrange
+            var customer = new Customer();
+            string goalSteps = "5000";
+            string actualSteps = "2000";
+            decimal expected = 40M;
+
+            //-- Act
+            var actual = customer.CalculatePercentOfGoalSteps(goalSteps, actualSteps);
+
+            //-- Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void CalculatePercentOfGoalStepsTestValidAndSame()
+        {
+            //-- Arrange
+            var customer = new Customer();
+            string goalSteps = "5000";
+            string actualSteps = "5000";
+            decimal expected = 100M;
+
+            //-- Act
+            var actual = customer.CalculatePercentOfGoalSteps(goalSteps, actualSteps);
+
+            //-- Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void CalculatePercentOfGoalStepsTestValidActualIsZero()
+        {
+            //-- Arrange
+            var customer = new Customer();
+            string goalSteps = "5000";
+            string actualSteps = "0";
+            decimal expected = 0M;
+
+            //-- Act
+            var actual = customer.CalculatePercentOfGoalSteps(goalSteps, actualSteps);
+
+            //-- Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void CalculatePercentOfGoalStepsTestGoalIsNull()
+        {
+            //-- Arrange
+            var customer = new Customer();
+            string goalSteps = null;
+            string actualSteps = "2000";
+
+            //-- Act
+            var actual = customer.CalculatePercentOfGoalSteps(goalSteps, actualSteps);
+
+            //-- Assert
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void CalculatePercentOfGoalStepsTestGoalIsNotNumeric()
+        {
+            //-- Arrange
+            var customer = new Customer();
+            string goalSteps = "one";
+            string actualSteps = "2000";
+
+            //-- Act
+            try
+            {
+                var actual = customer.CalculatePercentOfGoalSteps(goalSteps, actualSteps);
+            }
+            catch (Exception ex)
+            {
+              Assert.AreEqual("Goal must be numeric\r\nParameter name: goalSteps", ex.Message);
+                throw;
+            }
+
+            //-- Assert
+        }
+    }
+}
