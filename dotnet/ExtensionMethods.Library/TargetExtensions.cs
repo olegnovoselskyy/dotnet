f5181ad886fb8f3462076d7e65c9caad03f2d81c
Dmytro Lytvyn <dlytpi@gmail.com>
2019-11-19 00:16:04 -0500

    RD-25 Hiding Extension Methods

---
+++ dotnet/ExtensionMethods.Library/TargetExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExtensionMethods.Library
+{
+    public static class TargetExtensions
+    {
+        public static string GetStandardizedId(this Target target)
+        {
+            return target.GetId().ToUpper();
+        }
+    }
+}
