a9670899380fb73d3c0a704881882c566ef99a04
Oleg Novoselskyy <oleg.novoselskyy@gmail.com>
2019-11-29 21:08:37 -0500

    RD-10

--- DotNet/OdeToFood.Web/Global.asax.cs
+++ DotNet/OdeToFood.Web/Global.asax.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.Http;
 
 namespace OdeToFood.Web
 {
@@ -17,6 +18,7 @@ namespace OdeToFood.Web
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
             ContainerConfig.RegisterContainer();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
         }
     }
 }
