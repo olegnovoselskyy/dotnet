5053df07412b6a5680916d354942fe013704492b
Oleg Novoselskyy <oleg.novoselskyy@gmail.com>
2020-03-28 12:52:57 -0400

    RD-18

--- DotNet/DotNetCore/Pages/Restaurants/List.cshtml.cs
+++ DotNet/DotNetCore/Pages/Restaurants/List.cshtml.cs
@@ -4,13 +4,23 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 
 namespace DotNetCore.Pages.Restaurants
 {
     public class ListModel : PageModel
     {
+        public string Message { get; set; }
+        public IConfiguration Config { get; }
+
+        public ListModel(IConfiguration config)
+        {
+            Config = config;
+        }
+
         public void OnGet()
         {
+            Message = Config["Message"];
         }
     }
 }
